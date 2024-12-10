dd16d4f43b299b1688fe8c7bd39616cf160defe1
Samuel Ezra Nathan Andrew Sirait <samuelezra2013@gmail.com>
2024-12-10 20:16:28 +0800

    Squashed commit of the following:
    commit c087bc384f3da50abaac3b5b9676b7171b00bae3
    Author: Samuel Ezra Nathan Andrew Sirait <samuelezra2013@gmail.com>
    Date:   Tue Dec 10 19:00:04 2024 +0800
    
        Update route API #3
    
    commit 63c2a76f319f606d02fbf316e95b22e8e5772d05
    Author: Samuel Ezra Nathan Andrew Sirait <samuelezra2013@gmail.com>
    Date:   Tue Dec 10 18:36:52 2024 +0800
    
        Update route API #2
    
    commit a4e2e0d8d844f812d411a2524e05c404d5f01724
    Author: Samuel Ezra Nathan Andrew Sirait <samuelezra2013@gmail.com>
    Date:   Tue Dec 10 18:09:33 2024 +0800
    
        update API route

--- app/Http/Controllers/PembelianProdukController.php
+++ app/Http/Controllers/PembelianProdukController.php
@@ -64,4 +64,111 @@ class PembelianProdukController extends Controller
             'data' => $pembelian,
         ]);
     }
+
+    public function index()
+    {
+        $pembelian = PembelianProduk::with('detailPembelian')->get();
+        return response()->json($pembelian);
+    }
+
+    public function show($id)
+    {
+        $pembelian = PembelianProduk::with('detailPembelian')->find($id);
+
+        if (!$pembelian) {
+            return response()->json(['error' => 'Data pembelian tidak ditemukan'], 404);
+        }
+
+        return response()->json($pembelian);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $pembelian = PembelianProduk::find($id);
+
+        if (!$pembelian) {
+            return response()->json(['error' => 'Data pembelian tidak ditemukan'], 404);
+        }
+
+        $request->validate([
+            'id_user' => 'required',
+            'produk' => 'required|array',
+            'produk.*.id_produk' => 'required|exists:tb_produk,id_produk',
+            'produk.*.jumlah_produk' => 'required|integer|min:1',
+            'potongan_harga' => 'nullable|numeric|min:0',
+        ]);
+
+        $harga_total = 0;
+        $detail_produk = [];
+
+        // Kembalikan stok produk sebelumnya
+        foreach ($pembelian->detailPembelian as $detail) {
+            $produk = Produk::find($detail->id_produk);
+            $produk->increment('stok_produk', $detail->jumlah_produk);
+        }
+
+        // Hapus detail pembelian lama
+        $pembelian->detailPembelian()->delete();
+
+        foreach ($request->produk as $item) {
+            $produk = Produk::find($item['id_produk']);
+
+            if ($produk->stok_produk < $item['jumlah_produk']) {
+                return response()->json(['error' => 'Stok produk tidak mencukupi'], 400);
+            }
+
+            $subtotal = $item['jumlah_produk'] * $produk->harga_produk;
+            $harga_total += $subtotal;
+
+            $detail_produk[] = [
+                'id_produk' => $item['id_produk'],
+                'jumlah_produk' => $item['jumlah_produk'],
+                'harga_pembelian_produk' => $produk->harga_produk,
+            ];
+
+            $produk->decrement('stok_produk', $item['jumlah_produk']);
+        }
+
+        $pembelian->update([
+            'id_user' => $request->id_user,
+            'harga_total' => $harga_total,
+            'potongan_harga' => $request->potongan_harga ?? $pembelian->potongan_harga,
+            'harga_akhir' => $harga_total - ($request->potongan_harga ?? $pembelian->potongan_harga),
+        ]);
+
+        foreach ($detail_produk as $detail) {
+            DetailPembelianProduk::create([
+                'id_pembelian_produk' => $pembelian->id_pembelian_produk,
+                'id_produk' => $detail['id_produk'],
+                'jumlah_produk' => $detail['jumlah_produk'],
+                'harga_pembelian_produk' => $detail['harga_pembelian_produk'],
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data pembelian berhasil diperbarui',
+            'data' => $pembelian,
+        ]);
+    }
+
+    public function destroy($id_pembelian_produk)
+    {
+        $pembelian = PembelianProduk::find($id_pembelian_produk);
+
+        if (!$pembelian) {
+            return response()->json(['error' => 'Data pembelian tidak ditemukan'], 404);
+        }
+
+        // Hapus semua detail pembelian terkait
+        $pembelian->detailPembelian()->delete();
+
+        // Hapus data pembelian
+        $pembelian->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data pembelian berhasil dihapus',
+        ]);
+    }
 }
