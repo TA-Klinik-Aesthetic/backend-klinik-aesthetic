ebe686e402a36633157610e49de21d3509226924
NataNaraNPS <natanaraps@gmail.com>
2025-06-24 22:45:27 +0800

    tambah sedikit di bagian logout

--- routes/api.php
+++ routes/api.php
@@ -54,7 +54,7 @@ Route::post('/login', [AuthController::class, 'login']);
 
 // Route::middleware('auth:sanctum')->group(function () {
 // Logout
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 //informasi tiap entitas{
 Route::get('/users', [UserController::class, 'index']);
